56b765875baafdd1052415c601e204a9a204285f
Marchkkkk <104899636+Marchkkkk@users.noreply.github.com>
2022-06-07 14:55:07 +0300

    Add files via upload

---
+++ Nicknamegen.py
@@ -0,0 +1,36 @@
+import random
+
+Numberlist = []
+
+for x in range(1, 99):
+    Numberlist.append(random.randint(1, 100))
+
+amount = int (input('Amount of nicknames: '))
+
+f = open('Nicknames.txt','w+')
+
+with open("myfile.txt") as inp:
+    lines = inp.readlines()
+
+for x in range(1,amount +1):
+    part1 = random.choice(lines).strip()
+    part2 = random.choice(lines).strip()
+    part3 = str (random.choice(Numberlist))
+
+    Nickname = part1+part2+part3
+
+    f.write(Nickname + '\n')
+    print(Nickname)
+
+f.close()
+
+
+
+
+
+
+
+
+
+
+
